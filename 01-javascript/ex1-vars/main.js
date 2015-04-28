f2e9f1e339a86348eb4cb0e291368a718d214ad7
Cold Head <certainthings@gmail.com>
2015-04-28 12:06:01 +1000

    finished ex1

--- 01-javascript/ex1-vars/main.js
+++ 01-javascript/ex1-vars/main.js
@@ -34,13 +34,56 @@ var possibleAge2 = possibleAge1 - 1; // Before their birthday.
 
 console.log("They are either " + possibleAge2 + " or " + possibleAge1);
 
+// The Lifetime Supply Calculator
+
+// Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more!
+
+// Store your current age into a variable.
+// Store a maximum age into a variable.
+// Store an estimated amount per day (as a number).
+// Calculate how many you would eat total for the rest of your life.
+// Output the result to the screen like so: "You will need NN to last you until the ripe old age of X".
+
+var currentAge = 47;
+var deathAge = 62;
+var amountPerDay = 12;
+var amountPerYear = amountPerDay * 365.25;
+var yearsRemaining = deathAge - currentAge;
+var amountRequired = amountPerYear * yearsRemaining;
+console.log("You will need " + amountRequired +" to last you until the ripe old age of " + deathAge);
+
+// The Geometrizer
+
+// Calculate properties of a circle, using the definitions here.
+
+// Store a radius into a variable.
+// Calculate the circumference based on the radius, and output "The circumference is NN".
+// Calculate the area based on the radius, and output "The area is NN".
+
+var radius = 17;
+var circumference = Math.PI * radius * 2;
+var area = Math.PI * radius * radius;
+
+console.log("The circumference is " + circumference);
+console.log("The area is " + area);
 
 
+// The Temperature Converter
 
+// It's hot out! Let's make a converter based on the steps here.
 
+// Store a celsius temperature into a variable.
+// Convert it to fahrenheit and output "NN°C is NN°F".
+// Now store a fahrenheit temperature into a variable.
+// Convert it to celsius and output "NN°F is NN°C."
 
+var celcius = 20;
+var fahrenheit = celcius * 1.8 + 32;
+console.log(celcius + "°C is " + fahrenheit + "°F");
 
-
+var fahrenheit = 68;
+var celcius = (fahrenheit - 32) / 1.8;
+console.log(fahrenheit + "°F is " + celcius + "°C");
 
 
 
