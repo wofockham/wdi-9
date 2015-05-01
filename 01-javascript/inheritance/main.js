f14df4975af1ed65c1f9dab3b91f304928cd97a8
Cold Head <certainthings@gmail.com>
2015-05-01 11:19:52 +1000

    cute cats

---
+++ 01-javascript/inheritance/main.js
@@ -0,0 +1,48 @@
+/*
+ _____      _               _ _
+|_   _|    | |             (_) |
+  | | _ __ | |__   ___ _ __ _| |_ __ _ _ __   ___ ___
+  | || '_ \| '_ \ / _ \ '__| | __/ _` | '_ \ / __/ _ \
+ _| || | | | | | |  __/ |  | | || (_| | | | | (_|  __/
+ \___/_| |_|_| |_|\___|_|  |_|\__\__,_|_| |_|\___\___| Code along
+
+*/
+
+var lizzieTheCat = {
+  age: 18,
+  furColor: 'grey',
+  name: 'Lizzie',
+  meow: function() {
+    console.log(this.name + ' says: meowww');
+  }
+};
+
+var cuteCats = [
+  {
+    age: 18,
+    furColor: 'grey',
+    name: 'Lizzie',
+    meow: function() {
+      console.log(this.name + ' says: meowww');
+    }
+  },
+
+  {
+    age: 1,
+    furColor: 'black',
+    name: 'Daemon',
+    meow: function() {
+      console.log(this.name + ' says: meowww');
+    }
+  },
+
+  {
+    age: 5,
+    furColor: 'white',
+    name: 'Benji',
+    meow: function() {
+      console.log(this.name + ' says: meowww');
+    }
+  }
+];
+
