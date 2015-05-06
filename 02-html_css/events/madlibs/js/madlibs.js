141af94297c3d4f8d5f7bf14e97bcea5fa56d457
Cold Head <certainthings@gmail.com>
2015-05-06 15:53:03 +1000

    pink cucumbers

---
+++ 02-html_css/events/madlibs/js/madlibs.js
@@ -0,0 +1,13 @@
+var button = document.getElementById('lib-button');
+
+var makeMadLib = function () {
+  var noun = document.getElementById('noun').value;
+  var adjective = document.getElementById('adjective').value;
+  var person = document.getElementById('person').value;
+
+  var story = [person, 'really likes', adjective, noun].join(' ');
+
+  document.getElementById('story').innerHTML = story;
+};
+
+button.addEventListener('click', makeMadLib);
