bedc7b373dca756701dbd7ff55713b702e9767d8
Cold Head <certainthings@gmail.com>
2015-05-13 16:37:19 +1000

    tweening

--- 03-jquery/animations/js/tween.js
+++ 03-jquery/animations/js/tween.js
@@ -1,3 +1,5 @@
+var currentTween;
+
 $(document).ready(function () {
 
   // Traditional jQuery animation:
@@ -10,5 +12,28 @@ $(document).ready(function () {
   // TweenMax method:
   // TweenMax.to( selector, timeInSeconds, object );
 
-  TweenMax.to( '.image', 0.5, { left: '50%' });
+  var animateImage = function () {
+    currentTween = TweenMax.to( '.image', 3, {
+      backgroundColor: 'pink',
+      left: '50%',
+      top: '50%',
+      width: 600,
+      xPosition: '-50%',
+      yPosition: '-50%',
+      rotation: '1080',
+      delay: 1,
+      yoyo: true,
+      repeat: -1,
+      repeatDelay: 0.5,
+      ease: Bounce.easeOut,
+      onStart: function () {
+        // console.log('Animation Started');
+      },
+      onUpdate: function () {
+        // console.log('Animation updated');
+      }
+    });
+  };
+
+  $('html').on('click', animateImage);
 });
