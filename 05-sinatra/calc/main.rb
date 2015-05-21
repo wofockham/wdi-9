4c5f2d66b07854b0fbe625bac59c3c666f7aed3b
Cold Head <certainthings@gmail.com>
2015-05-21 12:31:14 +1000

    stupid gombuters

--- 05-sinatra/calc/main.rb
+++ 05-sinatra/calc/main.rb
@@ -14,13 +14,3 @@ get '/calc' do
 
   erb :calc
 end
-
-get '/calc/multiply/:x/:y' do
-  @result = params[:x].to_f * params[:y].to_f
-  erb :calc
-end
-
-get '/calc/add/:x/:y' do
-  @result = params[:x].to_f + params[:y].to_f
-  erb :calc
-end
