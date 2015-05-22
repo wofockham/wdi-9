ac4e4df9172a3cff95355a7f8dba1e68358cac9b
Cold Head <certainthings@gmail.com>
2015-05-22 12:03:10 +1000

    CRUD CRUD CRUD

---
+++ 06-sql/butterflies/main.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sqlite3'
+
+get '/' do
+  erb :home
+end
+
+get '/butterflies' do
+  db = SQLite3::Database.new 'database.db'
+  db.results_as_hash = true
+  @butterflies = db.execute "SELECT * FROM butterflies"
+
+  erb :butterflies_index
+end
+
+get '/butterflies/:id' do
+  db = SQLite3::Database.new 'database.db'
+  db.results_as_hash = true
+  @butterfly = db.execute "SELECT * FROM butterflies WHERE id = #{ params[:id] }"
+  @butterfly = @butterfly.first # Get the single butterfly out of the array.
+
+  erb :butterflies_show
+end
+
+
+
+
