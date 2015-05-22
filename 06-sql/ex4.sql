70a5f29b0b194541437f9a4f249c96d36cecc4d9
Cold Head <certainthings@gmail.com>
2015-05-22 21:49:24 +1000

    sql examples

---
+++ 06-sql/ex4.sql
@@ -0,0 +1,2 @@
+INSERT INTO people_pets (person_id, pet_id) VALUES (1, 1);
+INSERT INTO people_pets VALUES (1, 2);
---
+++ 06-sql/ex5.sql
@@ -0,0 +1,2 @@
+UPDATE people SET age=38 WHERE id = 1;
+DELETE FROM pets WHERE id = 2;
