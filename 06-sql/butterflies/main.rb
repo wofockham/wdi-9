e52be9d4e9fcf3373ad4e325f37c889a77b15fdf
Cold Head <certainthings@gmail.com>
2015-05-22 14:56:14 +1000

    butterfly CRUD

--- 06-sql/butterflies/main.rb
+++ 06-sql/butterflies/main.rb
@@ -7,11 +7,14 @@ get '/' do
 end
 
 get '/butterflies' do
-  db = SQLite3::Database.new 'database.db'
-  db.results_as_hash = true
-  @butterflies = db.execute "SELECT * FROM butterflies"
-
+  @butterflies = db_query "SELECT * FROM butterflies"
   erb :butterflies_index
+end
+
+post '/butterflies' do
+  q = "INSERT INTO butterflies (name, family, image) VALUES ('#{ params['name'] }', '#{ params['family'] }', '#{ params['image'] }')"
+  db_query q
+  redirect to('/butterflies')
 end
 
 get '/butterflies/new' do
@@ -19,11 +22,42 @@ get '/butterflies/new' do
 end
 
 get '/butterflies/:id' do
-  db = SQLite3::Database.new 'database.db'
-  db.results_as_hash = true
-  @butterfly = db.execute "SELECT * FROM butterflies WHERE id = #{ params[:id] }"
+  @butterfly = db_query "SELECT * FROM butterflies WHERE id = #{ params[:id] }"
   @butterfly = @butterfly.first # Get the single butterfly out of the array.
-
   erb :butterflies_show
 end
+
+get '/butterflies/:id/edit' do
+  @butterfly = db_query "SELECT * FROM butterflies WHERE id = #{ params[:id] }"
+  @butterfly = @butterfly.first
+  erb :butterflies_edit
+end
+
+post '/butterflies/:id' do
+  q = "UPDATE butterflies SET name='#{ params[:name] }', family='#{ params[:family] }', image='#{ params[:image] }' WHERE id=#{params[:id]}"
+  db_query q
+  redirect to("/butterflies/#{ params[:id] }")
+end
+
+get '/butterflies/:id/delete' do
+  db_query "DELETE FROM butterflies WHERE id = #{ params[:id] }"
+  redirect to('/butterflies')
+end
+
+def db_query(sql)
+  db = SQLite3::Database.new 'database.db'
+  db.results_as_hash = true
+
+  result = db.execute sql
+
+  db.close
+
+  result
+end
+
+
+
+
+
+
 
