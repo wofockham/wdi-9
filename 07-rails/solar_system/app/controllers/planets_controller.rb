9ba27517ad4279632bb2f7242fe1fca075ecc61a
Cold Head <certainthings@gmail.com>
2015-05-26 16:26:47 +1000

    CRUD CRUD CRUD CRUD CRUD

--- 07-rails/solar_system/app/controllers/planets_controller.rb
+++ 07-rails/solar_system/app/controllers/planets_controller.rb
@@ -15,6 +15,22 @@ class PlanetsController < ApplicationController
     @planet = Planet.find params[:id]
   end
 
+  def destroy
+    planet = Planet.find params[:id]
+    planet.destroy
+    redirect_to planets_path
+  end
+
+  def edit
+    @planet = Planet.find params[:id]
+  end
+
+  def update
+    planet = Planet.find params[:id]
+    planet.update planet_params
+    redirect_to planets_path
+  end
+
   private
   def planet_params # Rails 4 Params White Listing
     params.require(:planet).permit(:name, :image, :orbit, :diameter, :mass, :moons)
