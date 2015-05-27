15c81a3e759aea03c6200e3dece61ca30766013e
Cold Head <certainthings@gmail.com>
2015-05-27 11:19:38 +1000

    controller skellingtons

---
+++ 07-rails/moma_app/app/controllers/works_controller.rb
@@ -0,0 +1,22 @@
+class WorksController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
