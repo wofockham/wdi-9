6f8d08d6abd502252f408eb263c00eefa3ffe2ea
Cold Head <certainthings@gmail.com>
2015-05-29 10:13:57 +1000

    logout works

--- 07-rails/tunr/config/routes.rb
+++ 07-rails/tunr/config/routes.rb
@@ -14,7 +14,13 @@
 
 Rails.application.routes.draw do
   root :to => 'pages#home'
+
   get '/haml' => 'pages#hamldemo'
 
   resources :users
+
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
+
 end
