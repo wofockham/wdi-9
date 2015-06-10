f792b64e05b09d4aa2e822d4cbff21a7ed2be898
Cold Head <certainthings@gmail.com>
2015-06-10 15:02:03 +1000

    pregnant butterflies indeed

--- 08-ajax/flickr/app/assets/javascripts/search.js
+++ 08-ajax/flickr/app/assets/javascripts/search.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
+
+  $(window).on('scroll', function () {
+    console.log($(window).scrollTop(), $(document).height());
+  });
+
   $('#image_search').on('submit', function (e) {
     e.preventDefault();
 
@@ -26,11 +31,13 @@ $(document).ready(function () {
       method: 'flickr.photos.search',
       api_key: '2f5ac274ecfac5a455f38745704ad084',
       text: query,
-      format: 'json'
+      format: 'json',
+      per_page: 100
     }).done(function (results) {
       $.each(results.photos.photo, function (i, photo) {
         var url = generateUrl(photo);
-        console.log(url);
+        var $img = $('<img>').addClass('thumbnail').attr('src', url);
+        $img.appendTo('body');
       })
     });
 
