c12c78279d1567ef333a22b62220f0a262d1d8ad
Cold Head <certainthings@gmail.com>
2015-06-11 10:57:07 +1000

    infinite scroll

--- 08-ajax/flickr/app/assets/javascripts/search.js
+++ 08-ajax/flickr/app/assets/javascripts/search.js
@@ -1,24 +1,26 @@
 $(document).ready(function () {
 
-  $(window).on('scroll', function () {
-    console.log($(window).scrollTop(), $(document).height());
-    // Your infinite scroll logic goes here...
-  });
+  var pageNumber = 1;
+  var requestInProgress = false;
 
-  $('#image_search').on('submit', function (e) {
-
-    e.preventDefault();
-
+  var fetchImages = function () {
     var query = $('#search').val();
     var flickrUrl = 'https://api.flickr.com/services/rest/?jsoncallback=?';
+
+    console.log('fetching images', query, pageNumber);
+
+    requestInProgress = true;
 
     $.getJSON(flickrUrl, {
       method: 'flickr.photos.search', // See the docs for other interesting methods.
       api_key: '2f5ac274ecfac5a455f38745704ad084',
       text: query,
       format: 'json',
-      per_page: 500
+      per_page: 40,
+      page: pageNumber++
     }).done(function (results) {
+
+      requestInProgress = false;
 
       // Returns a crufty Flickr thumbnail URL for a given photo object.
       var generateUrl = function (photo) {
@@ -36,6 +38,25 @@ $(document).ready(function () {
         $results.append($img);
       })
     });
+  };
+
+  $(window).on('scroll', function () {
+    if (requestInProgress) {
+      return;
+    }
+
+    var reloadRegion = $(document).height() * 0.8; // 80% of the way through.
+
+    if ($(window).height() + $(window).scrollTop() > reloadRegion) {
+      fetchImages();
+    }
+
+  });
+
+  $('#image_search').on('submit', function (e) {
+
+    e.preventDefault();
+    fetchImages();
 
   });
 });
