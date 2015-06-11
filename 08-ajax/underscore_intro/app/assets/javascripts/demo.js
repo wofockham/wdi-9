9b477eb29093db70aa23d5c81eff9c376e151ea7
Cold Head <certainthings@gmail.com>
2015-06-11 12:18:57 +1000

    squarey

--- 08-ajax/underscore_intro/app/assets/javascripts/demo.js
+++ 08-ajax/underscore_intro/app/assets/javascripts/demo.js
@@ -1,18 +1,30 @@
 var numbers = [];
 
+var showNumbers = function () {
+  $('#output').empty();
+  _(numbers).each(function (n) {
+    $('<div class="box"/>').text(n).prependTo('#output');
+  });
+};
+
 var addNumber = function () {
   var n = $('#number').val();
   n = ~~(n); // parseInt() that looks like a sperm.
   numbers.push(n);
 
-  var $box = $('<div class="box"/>').text(n);
-  $('#output').append($box);
+  showNumbers();
 
   $('#number').val('').focus();
+};
+
+var squareNumbers = function () {
+  numbers = _(numbers).map(function (n) { return n * n; });
+  showNumbers();
 };
 
 $(document).ready(function () {
 
   $('#add_number').on('click', addNumber);
+  $('#square').on('click', squareNumbers);
 
 });
