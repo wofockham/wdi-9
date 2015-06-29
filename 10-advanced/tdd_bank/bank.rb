83c69b8974f86fd87d83216c39328851b3e47b2c
Cold Head <certainthings@gmail.com>
2015-06-29 12:58:22 +1000

    #deposit

--- 10-advanced/tdd_bank/bank.rb
+++ 10-advanced/tdd_bank/bank.rb
@@ -1,4 +1,13 @@
 class Bank
+  attr_reader :name, :accounts
+
   def initialize(name)
+    @accounts = {}
+    @name = name
   end
+
+  def create_account(name, starting_deposit)
+    @accounts[name] = starting_deposit
+  end
+
 end
--- 10-advanced/tdd_bank/spec/bank_spec.rb
+++ 10-advanced/tdd_bank/spec/bank_spec.rb
@@ -1,15 +1,44 @@
 require_relative '../bank'
 
 describe Bank do
+  let(:bank) { Bank.new('TDD Bank') }
+
   describe '.new' do
     it 'creates a new Bank object' do
-      bank = Bank.new('TDD Bank')
       expect(bank).to_not eq nil
     end
 
     it 'has a name' do
-      bank = Bank.new('TDD Bank')
       expect(bank.name).to eq 'TDD Bank'
+    end
+
+    it 'has no accounts' do
+      expect(bank.accounts.count).to eq 0
+    end
+  end
+
+  describe '#create_account' do
+    it 'creates an account' do
+      bank.create_account('Jonesy', 200)
+      expect(bank.accounts['Jonesy']).to eq 200
+    end
+  end
+
+  describe '#deposit' do
+    it 'deposits money from client into their account' do
+      bank.create_account('Jonesy', 200)
+      bank.deposit('Jonesy', 300)
+      expect(bank.accounts['Jonesy']).to eq 200 + 300
     end
   end
 end
+
+
+
+
+
+
+
+
+
+
