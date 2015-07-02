aab21e85c9ed7d7ba4da5caab9243ca43fc706ad
Cold Head <certainthings@gmail.com>
2015-07-02 11:46:05 +1000

    kill me

---
+++ 10-advanced/calculator/features/step_definitions/calculator_steps.rb
@@ -0,0 +1,15 @@
+Given(/^I have a calculator$/) do
+  @calculator = Calculator.new
+end
+
+Given(/^I have the number (\d+)$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I add them together$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see (\d+) on the terminal$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
