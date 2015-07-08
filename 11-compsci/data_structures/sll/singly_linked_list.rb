19071d2ae052f524c477bee37fafb71a865ddd92
Cold Head <certainthings@gmail.com>
2015-07-08 10:48:17 +1000

    enumerable mixin

--- 11-compsci/data_structures/sll/singly_linked_list.rb
+++ 11-compsci/data_structures/sll/singly_linked_list.rb
@@ -1,66 +1,99 @@
-class Node
-  attr_accessor :value, :next
-
-  def initialize(value=nil)
-    @value = value
-    @next = nil
-  end
-end
-
 class SinglyLinkedList
+
+  include Enumerable
+
   attr_accessor :head
 
-  def initialize(first_value=nil)
-    @head = Node.new(first_value) if first_value
+  def initialize(value=nil)
+    @head = Node.new(value) if value
   end
 
   def prepend(value)
     node = Node.new(value)
     node.next = @head
     @head = node
+    self
   end
 
-  def append
-  end
-
-  # Returns the last node
   def last
+    node = @head
+    while node && node.next
+      node = node.next
+    end
+    node
   end
 
   def insertAfter(node, new_node)
+    new_node.next = node.next
+    node.next = new_node
   end
 
-  # Returns the (integer) number of nodes in this list.
+  def append(value)
+    last.next = Node.new(value)
+  end
+
+  def remove
+    node = @head
+    @head = node.next
+    node
+  end
+
   def length
+    tally = 0
+    node = @head
+    while node.respond_to? :next
+      node = node.next
+      tally += 1
+    end
+    tally
   end
 
-  # Returns the node with needle as value or nil.
   def find(needle)
+    node = @head
+    until node.nil?
+      return node if node.value == needle
+      node = node.next
+    end
+    nil
   end
 
   def reverse
+    reverse_list = SinglyLinkedList.new
+    node = @head
+    while node
+      reverse_list.prepend(node.value)
+      node = node.next
+    end
+    reverse_list
   end
 
   def reverse!
+    @head = reverse.head
+    self
   end
 
-  # Extra credit
   def each
+    node = @head
+    while node
+      yield node.value
+      node = node.next
+    end
+  end
+
+  # def to_s
+    #   self.map { |node| node }.join(', ')
+  # end
+
+  class Node
+    attr_accessor :value, :next
+
+    def initialize(value=nil)
+      @value = value
+      @next = nil
+    end
+
+    def to_s
+      @value || nil
+    end
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
