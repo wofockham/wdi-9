00d679d216b31d79447fc334e334e60ffc7a4c47
Cold Head <certainthings@gmail.com>
2015-07-15 15:12:01 +1000

    eerily well

---
+++ 10-advanced/angular/js/todo.js
@@ -0,0 +1,25 @@
+angular.module('todoApp', [])
+  .controller('TodoListController', function () {
+
+    var todoList = this;
+
+    todoList.todos = [
+      {text: 'learn angular', done: true},
+      {text: 'build an app', done: false}
+    ];
+
+
+    todoList.addTodo = function () {
+      todoList.todos.push({text: todoList.todoText, done: false});
+      todoList.todoText = '';
+    };
+
+    todoList.remaining = function () {
+      var count = 0;
+      angular.forEach(todoList.todos, function(todo) {
+        count += todo.done ? 0 : 1;
+      });
+      return count;
+    };
+
+  })
